1174b85826332b45a2e6b8e7fe41c47c30f961b4
andre.slavescu@gmail.com <andre.slavescu@gmail.com>
2021-07-21 21:00:46 +0000

    independant practice

--- starter/secretMessageApp/public/js/viewMessages.js
+++ starter/secretMessageApp/public/js/viewMessages.js
@@ -12,22 +12,34 @@ const getMessages = () => {
 
 getMessages();
 
+ctr = 0;
+
 const findMessage = (myPass) => {
     const messagesRef = firebase.database().ref();
     messagesRef.on('value', (snapshot) => {
         const data = snapshot.val();
-        console.log(data);
+        console.log(data);    
         for(let key in data) {
             const message = data[key];
             console.log(message);
-            if (myPass == message.passcode) {
-                renderMessage(message);
+            if (myPass == message.password) {
+                console.log(myPass);
+            } else {
+                if (ctr >= 5) {
+                    alert('Password wrong too many times');
+                    location.reload();
+                    break;
+                } else if (ctr < 5 && myPass.length > 0) {
+                    ctr ++;
+                    alert("Wrong Password, try again. you have " + ctr + " wrong attempts");
+                    break;
+                } 
             }
         }
     });
 }
 
-document.querySelector("#viewMsg").addEventListener("click", () => {
-    const passcode = document.querySelector("#passcode").value;
-    findMessage(passcode);
+document.querySelector("#viewMsg").addEventListener("click", ()=>{
+    const password = document.querySelector("#passcode").value;
+    findMessage(password);
 });
